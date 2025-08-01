4d8fd1a3e5018cf678d6373cd706878f984f23ed
Abyan Dimas <abyandimas@Abyans-MacBook-Air.local>
2025-08-01 19:28:09 +0700

    update

--- src/app/(home)/donasi/page.tsx
+++ src/app/(home)/donasi/page.tsx
@@ -37,7 +37,7 @@ export default function DonasiPage() {
             </div>
             
             <p className="text-center text-gray-600 dark:text-gray-300 mb-4">
-              Scan QRIS di atas menggunakan aplikasi mobile banking atau e-wallet Anda
+              Scan QRIS di atas menggunakan aplikasi mobile banking atau e-wallet Anda atau <strong>Transfer Bank Rekening <strong className='text-emerald-300'>BSI 7998006664</strong></strong> a.n. PANITIA REHAB MASJID KHOIRUL BAI
             </p>
             
             <div className="bg-yellow-50 dark:bg-yellow-900/20 border border-yellow-200 dark:border-yellow-800 rounded-lg p-3 w-full max-w-md">
